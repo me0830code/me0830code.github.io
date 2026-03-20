4999b55b59cc1810a203cba9cf9e27882a0d4de1
me0830code <me0830code@gmail.com>
2026-03-20 21:50:35 +0800

    Update title and company

--- src/component/leftSideBar/ProfileInfo.tsx
+++ src/component/leftSideBar/ProfileInfo.tsx
@@ -8,30 +8,22 @@ export function ProfileInfo() {
             </h2>
             
             <p className="mt-3 text-base text-sky-600 font-semibold">
-                {PERSONAL_INFO.CURRENT_TITLE} at <a>123</a>
-            </p>
-            
-            <div>
-                <span className="mt-3 text-base text-sky-600 font-semibold">
-                    at
-                </span>
-
+                {PERSONAL_INFO.CURRENT_TITLE} at <></>
                 <a
       href="https://www.google.com"
       target="_blank"
       rel="noopener noreferrer"
       aria-label={PERSONAL_INFO.COMPANY}
       title={PERSONAL_INFO.COMPANY}
-      className="inline-flex items-center transition hover:opacity-80"
-    >coupang
+      className="inline-flex items-center transition hover:opacity-80 text-black font-semibold"
+    >Coupang
       {/* <img
         src="https://upload.wikimedia.org/wikipedia/commons/3/35/Coupang_logo.png"
         alt={PERSONAL_INFO.COMPANY}
         className="h-5 w-auto object-contain"
       /> */}
     </a>
-            </div>
-            
+    </p>
             <p className="mt-3 max-w-xs text-sm leading-7 text-slate-600">
                 {PERSONAL_INFO.BRIEF_INTRO}
             </p>
