d443488bbbf95736d42549c641492a150f9088b2
me0830code <me0830code@gmail.com>
2026-03-24 23:22:40 +0800

    Update skillSet data

---
+++ src/data/skillSet/dataBase.ts
@@ -0,0 +1,14 @@
+export const dataBase = [
+    { name: "SQL", iconURL: "devicon:azuresqldatabase" },
+    { name: "NoSQL", iconURL: "material-icon-theme:database" },
+
+    { name: "MySQL", iconURL: "skill-icons:mysql-light" },
+    { name: "DynamoDB", iconURL: "skill-icons:dynamodb-light" },
+    { name: "MongoDB", iconURL: "skill-icons:mongodb" },
+    { name: "Redis", iconURL: "skill-icons:redis-light" },
+    { name: "Firebase", iconURL: "devicon:firebase" },
+
+    { name: "GraphQL", iconURL: "skill-icons:graphql-light" },
+
+    { name: "Neo4j", iconURL: "devicon:neo4j" },
+]
---
+++ src/data/skillSet/programmingLanguage.ts
@@ -0,0 +1,15 @@
+export const programmingLanguage = [
+    { name: "Java", iconURL: "skill-icons:java-light" },
+    { name: "Kotlin", iconURL: "skill-icons:kotlin-light" },
+    { name: "Python", iconURL: "skill-icons:python-light" },
+    { name: "C", iconURL: "skill-icons:c" },
+    { name: "C++", iconURL: "skill-icons:cpp" },
+    { name: "C#", iconURL: "skill-icons:cs" },
+    { name: "Swift", iconURL: "skill-icons:swift" },
+    { name: "Object-C", iconURL: "logos:apple" },
+    { name: "JavaScript", iconURL: "skill-icons:javascript" },
+
+    { name: "React", iconURL: "skill-icons:swift" },
+    { name: "Vue", iconURL: "skill-icons:vuejs-light" },
+    { name: ".NET", iconURL: "skill-icons:dotnet" },
+]
