0c9d6caadc173d4796847b2e9fffb8da15244b94
me0830code <me0830code@gmail.com>
2026-04-08 23:44:26 +0800

    Update resume button bg color

--- src/component/RightContent/AboutMe/ViewMyResume.tsx
+++ src/component/RightContent/AboutMe/ViewMyResume.tsx
@@ -8,7 +8,7 @@ export function ViewMyResume() {
     };
 
     return (
-        <button onClick={handleOpenResume} className="group inline-flex h-11 w-full items-center justify-center gap-2 rounded-md border-2 border-black bg-[#fff0de] px-4 text-sm font-medium text-black transition-all duration-200 hover:text-sky-600 md:h-12 md:w-auto md:px-6 md:text-[16px]">
+        <button onClick={handleOpenResume} className="group inline-flex h-11 w-full items-center justify-center gap-2 rounded-md border-2 border-black bg-[#ead9ff] px-4 text-sm font-medium text-black transition-all duration-200 hover:text-sky-600 md:h-12 md:w-auto md:px-6 md:text-[16px]">
             <FileText size={18} className="transition-transform duration-200 group-hover:scale-110 md:size-5"/>
             <span className="whitespace-nowrap">
                 View My Resume
